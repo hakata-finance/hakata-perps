23006fb89f92e0765bc0cac28281c57de15663dc
IhorMuliar <goga440@gmail.com>
2025-05-07 17:38:50 +0300

    Add state logic

--- programs/hakata-perpetuals/src/lib.rs
+++ programs/hakata-perpetuals/src/lib.rs
@@ -1,8 +1,20 @@
 #![allow(unexpected_cfgs)]
 
+pub mod error;
+pub mod math;
+pub mod state;
+
 use anchor_lang::prelude::*;
 
 declare_id!("6wCkVSLvphXyQphT8xDemkx92caLJXcaFF9qjxxL2o8Q");
+
+#[macro_export]
+macro_rules! try_from {
+    // https://github.com/coral-xyz/anchor/pull/2770
+    ($ty: ty, $acc: expr) => {
+        <$ty>::try_from(unsafe { core::mem::transmute::<_, &AccountInfo<'_>>($acc.as_ref()) })
+    };
+}
 
 #[program]
 pub mod hakata_perpetuals {
