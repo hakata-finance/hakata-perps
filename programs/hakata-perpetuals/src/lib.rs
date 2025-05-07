6b9797ec73248951c4219b6ca17464fff417f9fa
IhorMuliar <goga440@gmail.com>
2025-05-07 16:21:48 +0300

    Init commit

---
+++ programs/hakata-perpetuals/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6wCkVSLvphXyQphT8xDemkx92caLJXcaFF9qjxxL2o8Q");
+
+#[program]
+pub mod hakata_perpetuals {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
